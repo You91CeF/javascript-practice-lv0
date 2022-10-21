66413cbded95f4d13e2b5d3f0b772832b2c5ecba
Youcef <cefyou.blancos@gmail.com>
2022-10-21 16:45:33 +0200

    loops

--- app.js
+++ app.js
@@ -56,3 +56,181 @@ function pairimpair() {
         alert("The number " + number + " is impair")
     }
 }
+
+
+
+function findlargest() {
+    let num1 = prompt("give a num1");
+    let num2 = prompt("give a num2");
+    if (num1 > num2) {
+        return alert("the largest number is num1");
+    }
+    else if(num2>num1) {
+        return alert("the largest number is num2");
+    }
+    else {
+        return alert("num1 and num2 are equal");
+    }
+}
+
+function leapyear() {
+    let year = prompt("give a year");
+    let leap;
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        alert(year + " is a leap year");
+        return leap = true;
+    }
+    else {
+        alert(year + " not a leap year");
+        return leap = false;
+    }
+}
+
+function findDaysInMonth() {
+    let month = prompt("Give the number of month");
+    if (month <= 0 || month > 12) {
+        alert("Invalid month");
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        alert("30 days");
+    }
+    else if (month == 2){
+        let leap = leapyear();
+        if (leap == true){
+            alert("29 days");
+        }
+        else {
+            alert("28 days");
+        }
+    }
+    else {
+        alert("31 days")
+    }
+}
+function evalNumbers() {
+    let num1 = prompt("give value number 1:");
+    let num2 = prompt("give value number 2:");
+    let operation = prompt("What operation you want to do ?");
+    switch (operation) {
+        case "add" :
+            let add = Number(num1)+Number(num2);
+            console.log(add);
+            console.log(typeof(num1));
+            alert("the result is " + add);
+            break;
+        case "substract" :
+           let substract = num1 - num2;
+            alert("the result is " + substract);
+            break;
+        case "multiply" :
+            let multiply = num1 * num2;
+            alert("the result is " + multiply);
+            break;
+        case "divide" :
+            let divide = num1 / num2;
+            alert("the result is " + divide);
+            break;
+                    case "modulus" :
+            let modulus = num1 % num2;
+            alert("the result is " + modulus);
+            break;
+    }
+}
+
+function factoriel() {
+    let number = prompt("Give a number");
+    let i = 1;
+    let result = 1;
+    for (i =1; i <= number; i++){
+        result *= i;
+    }
+    console.log(result);
+    alert("the factorial of "+ number + " is " + result)
+}
+
+function For() {
+    for (let i = 1; i <= 100; i++) {
+        //console.log(i);
+        if ((i%3 == 0) && (i%5 ==0)){
+            console.log("FizzBuzz");
+        }
+        else if (i%3 == 0) 
+        {
+            console.log("Fizz");
+        }
+        else if (i%5 == 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+function While() {
+    let i = 1;
+    while(i <= 100) {
+        if ((i%3 == 0) && (i%5 ==0)){
+            console.log("FizzBuzz");
+        }
+        else if (i%3 == 0) 
+        {
+            console.log("Fizz");
+        }
+        else if (i%5 == 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    i++;
+    }
+}
+
+function DoWhile() {
+    let i = 0;
+    do {
+        i++;
+        if ((i%3 == 0) && (i%5 ==0)){
+            console.log("FizzBuzz");
+        }
+        else if (i%3 == 0) 
+        {
+            console.log("Fizz");
+        }
+        else if (i%5 == 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    } while(i <= 99);
+    
+}
+
+function tablemultiplication(){
+    let n = prompt("Give a number");
+    let result;
+    for (let i=1; i<=10;i++){
+        result = n * i;
+        console.log(n + " X " + i + " = " + result);
+    }
+}
+
+function primaryNumber() {
+    let n = prompt("give a number");
+    let nbdevide = 0;
+    let result;
+    for (let i=1; i<=n; i++) {
+        for (let j=1; j<=i;j++){
+            result = i%j;
+            if (result == 0) {
+                nbdevide++;
+            }
+        }
+        if (nbdevide == 2) {
+            console.log(n);
+        }
+    }
+}
+    
